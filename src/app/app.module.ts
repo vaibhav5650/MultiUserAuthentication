6639bdce700540c4329b1b38789a21ddbfbb3cb2
vaibhav5650 <vaibhavpanchbhai15@gmail.com>
2024-10-04 15:09:18 +0530

    This is my userAuthentication repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
